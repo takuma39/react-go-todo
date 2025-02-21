61f563bb59e63484412d067debd66c7c677d15c4
takuma39 <takuma.sato.job@gmail.com>
2025-02-21 23:54:48 +0900

    add:認証画面、todo画面

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -1,3 +1,83 @@
+import { useState, FormEvent } from 'react'
+import { CheckBadgeIcon, ArrowPathIcon } from '@heroicons/react/24/solid'
+import { useMutateAuth } from '../hooks/useMutateAuth'
+
 export const Auth = () => {
-  return <div>Auth</div>
+  const [email, setEmail] = useState('')
+  const [pw, setPw] = useState('')
+  const [isLogin, setIsLogin] = useState(true)
+  const { loginMutation, registerMutation } = useMutateAuth()
+
+  const submitAuthHandler = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    if (isLogin) {
+      loginMutation.mutate({
+        email: email,
+        password: pw,
+      })
+    } else {
+      await registerMutation
+        .mutateAsync({
+          email: email,
+          password: pw,
+        })
+        .then(() =>
+          loginMutation.mutate({
+            email: email,
+            password: pw,
+          })
+        )
+    }
+  }
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen font-mono text-gray-600">
+      <div className="flex items-center">
+        <CheckBadgeIcon className="w-8 h-8 mr-2 text-blue-500" />
+        <span className="text-3xl font-extrabold text-center">
+          Todo app by React/Go(Echo)
+        </span>
+      </div>
+      <h2 className="my-6">{isLogin ? 'Login' : 'Create a new account'}</h2>
+      <form onSubmit={submitAuthHandler}>
+        <div>
+          <input
+            className="px-3 py-2 mb-3 text-sm border border-gray-300"
+            name="email"
+            type="email"
+            autoFocus
+            placeholder="Email address"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
+        </div>
+        <div>
+          <input
+            className="px-3 py-2 mb-3 text-sm border border-gray-300"
+            name="password"
+            type="password"
+            placeholder="Password"
+            onChange={(e) => setPw(e.target.value)}
+            value={pw}
+          />
+        </div>
+        <div className="flex justify-center my-2">
+          <button
+            className="w-full px-4 py-2 text-white bg-indigo-600 rounded disabled:opacity-40"
+            disabled={!email || !pw}
+            type="submit"
+          >
+            {isLogin ? 'Login' : 'Sign Up'}
+          </button>
+        </div>
+      </form>
+      <div
+        onClick={() => setIsLogin(!isLogin)}
+        className="flex items-center gap-1 cursor-pointer"
+      >
+        <ArrowPathIcon className="w-6 h-6 my-2 text-blue-500 cursor-pointer" />
+        <span>{isLogin ? 'Sign Up' : 'Login'}</span>
+      </div>
+    </div>
+  )
 }
--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,3 +1,80 @@
+import { FormEvent } from 'react'
+import { useQueryClient } from '@tanstack/react-query'
+import {
+  ArrowRightOnRectangleIcon,
+  ShieldCheckIcon,
+} from '@heroicons/react/24/solid'
+import useStore from '../store'
+import { useQueryTasks } from '../hooks/useQueryTasks'
+import { useMutateTask } from '../hooks/useMutateTask'
+import { useMutateAuth } from '../hooks/useMutateAuth'
+import { TaskItem } from './TaskItem'
+
 export const Todo = () => {
-  return <div>Todo</div>
+  const queryClient = useQueryClient()
+  const { editedTask } = useStore()
+  const updateTask = useStore((state) => state.updateEditedTask)
+  const { data, isLoading } = useQueryTasks()
+  const { createTaskMutation, updateTaskMutation } = useMutateTask()
+  const { logoutMutation } = useMutateAuth()
+  const submitTaskHandler = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    if (editedTask.id === 0)
+      createTaskMutation.mutate({
+        title: editedTask.title,
+      })
+    else {
+      updateTaskMutation.mutate(editedTask)
+    }
+  }
+  const logout = async () => {
+    await logoutMutation.mutateAsync()
+    queryClient.removeQueries(['tasks'])
+  }
+  return (
+    <div className="flex flex-col p-5 font-mono text-gray-600">
+      <div className="flex items-center">
+        <ArrowRightOnRectangleIcon
+          onClick={logout}
+          className="w-6 text-blue-500 cursor-pointer"
+        />
+        ログアウト
+      </div>
+
+      <div className="flex flex-col items-center">
+        <div className="flex my-3 ">
+          <ShieldCheckIcon className="w-8 h-8 mr-3 text-indigo-500 cursor-pointer" />
+          <span className="text-3xl font-extrabold text-center">
+            Task Manager
+          </span>
+        </div>
+        <form onSubmit={submitTaskHandler}>
+          <input
+            className="px-3 py-2 mb-3 mr-3 border border-gray-300"
+            placeholder="title ?"
+            type="text"
+            onChange={(e) =>
+              updateTask({ ...editedTask, title: e.target.value })
+            }
+            value={editedTask.title || ''}
+          />
+          <button
+            className="px-3 py-2 mx-3 text-white bg-indigo-600 rounded disabled:opacity-40"
+            disabled={!editedTask.title}
+          >
+            {editedTask.id === 0 ? 'Create' : 'Update'}
+          </button>
+        </form>
+        {isLoading ? (
+          <p>Loading...</p>
+        ) : (
+          <ul className="my-5">
+            {data?.map((task) => (
+              <TaskItem key={task.id} id={task.id} title={task.title} />
+            ))}
+          </ul>
+        )}
+      </div>
+    </div>
+  )
 }
